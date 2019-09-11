14746321e2e2e639597564aaf4e3141fc457b265
rdanov <rdanov80@gmail.com>
2019-09-11 13:54:43 +0300

    WD_PS2 task1 completed

--- WD_PS2/Task1/js/calc.js
+++ WD_PS2/Task1/js/calc.js
@@ -2,41 +2,49 @@ window.onload = function () {
     const button = document.querySelector('.button_calculate');
     button.addEventListener('click', (e) => {
         console.log(e.target);
+
         sumOfNumbers();
     })
-}
+};
 
 function sumOfNumbers() {
     const parent = document.querySelector('.container');
+
     if (!parent) {
+        alert("enter numbers please");
         return;
     }
+
     const elements = [...parent.querySelectorAll('.my_input')];
     if (!elements || elements.length < 2) {
+        alert("enter numbers please");
         return;
     }
+
     const firstNumber = +elements[0].value;
     const secondNumber = +elements[1].value;
-    if (!firstNumber || !secondNumber) {
+    if (firstNumber.isInteger || secondNumber.isInteger) {
+        alert("enter numbers please");
         return;
     }
     const output = document.querySelector('.result');
     if (!output) {
+        alert("enter numbers please");
         return;
     }
     let summ_of_numbers = 0;
     if (firstNumber > secondNumber) {
         for (let i = secondNumber; i <= firstNumber; i++) {
-            if (i % 10 === 2 || i % 10 === 3 || i % 10 === 7) {
+            if (Math.abs(i) % 10 === 2 || Math.abs(i) % 10 === 3 || Math.abs(i) % 10 === 7) {
                 summ_of_numbers += i;
             }
         }
     } else {
         for (let i = firstNumber; i <= secondNumber; i++) {
-            if (i % 10 === 2 || i % 10 === 3 || i % 10 === 7) {
+            if (Math.abs(i) % 10 === 2 || Math.abs(i) % 10 === 3 || Math.abs(i) % 10 === 7) {
                 summ_of_numbers += i;
             }
         }
     }
-     output.innerText = secondNumber;
+    output.innerText = summ_of_numbers;
 }
