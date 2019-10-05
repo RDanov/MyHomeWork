3feb52ea5c494fa7c89180c8dc3b126c5678d7b0
rdanov <rdanov80@gmail.com>
2019-10-05 11:09:59 +0300

    wd_ps2 task6, fixed problems, input associated with label

--- WD_PS2/Task6/js/checkText.js
+++ WD_PS2/Task6/js/checkText.js
@@ -1,17 +1,24 @@
 window.onload = function () {
-    const button = document.querySelector('.button_check')
+    const button = document.querySelector('.button_check');
     button.addEventListener('click', e => {
         analisingText();
     })
 };
 
 function analisingText() {
-    const textArea = document.querySelector('.text_for_checked').value;
+    let textArea = document.querySelector('.text_for_checked').value;
     const regularOrText = document.querySelector('.my_input').value;
     if (!textArea || !regularOrText) {
         alert("Enter text and regular");
         return;
     }
+    const regex = new RegExp(regularOrText, 'g');
+    const array = [...textArea.matchAll(regex)];
+    for (let index = 0; index < array.length; index++) {
+        let temp = new RegExp(array[index], 'g');
+        textArea = textArea.replace(temp, '<mark>' + array[index] + '</mark>');
+    }
     const output = document.querySelector('.result');
-    output.innerHTML = textArea.replace(regularOrText, '<mark>$&</mark>');
+    output.innerHTML = textArea;
 }
+
