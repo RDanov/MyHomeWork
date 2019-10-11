53f669ac5fa4ea4b7648619101c1b161644f59c7
rdanov <rdanov80@gmail.com>
2019-10-11 14:22:11 +0300

    wd_ps2 task 2 fixed

--- WD_PS2/Task2/js/calc.js
+++ WD_PS2/Task2/js/calc.js
@@ -5,7 +5,7 @@ window.onload = function () {
     const buttonOne = document.querySelector('#button_first');
     buttonOne.addEventListener('click', (e) => {
         timeConversionFirst();
-    })
+    });
     const buttonSecond = document.querySelector('#button_second');
     buttonSecond.addEventListener('click', (e) => {
         timeConversionSecond();
@@ -56,14 +56,14 @@ function timeConversionSecond() {
     const timeElements = element.value.split(':');
     for (let i = 0; i < timeElements.length; i++) {
         timeElements[i] = +timeElements[i];
-        if (timeElements.length !== 3 || !Number.isInteger(timeElements[i])) {
+        if (!Number.isInteger(timeElements[i])) {
             alert("enter time in format HH:MM:SS");
             return;
         }
     }
     const output = document.querySelector('#secondResult');
-
-    const resultTime = timeElements[0] * SECONDS_IN_HOUR + timeElements[1] * SECONDS_IN_MINUTE + timeElements[2];
-    output.innerText = "times in seconds is " + resultTime + " seconds";
+    let resultTime = timeElements[0] * SECONDS_IN_HOUR + timeElements[1] * SECONDS_IN_MINUTE;
+    if (timeElements.length === 3) resultTime += timeElements[2];
+    output.innerText = "times is " + resultTime + " seconds";
 }
 
