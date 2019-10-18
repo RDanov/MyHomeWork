207cc6f3797d5d40ed7926a5812c0213917090df
rdanov <rdanov80@gmail.com>
2019-10-18 16:58:30 +0300

    wd_ps2 task2 format of the displayed time is fixed

--- WD_PS2/Task2/js/calc.js
+++ WD_PS2/Task2/js/calc.js
@@ -34,11 +34,15 @@ function timeConversionFirst() {
         return;
     }
 
-    const hours = Math.trunc(time / SECONDS_IN_HOUR);
+    let hours = Math.trunc(time / SECONDS_IN_HOUR);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
     time = time % SECONDS_IN_HOUR;
     const minutes = Math.trunc(time / SECONDS_IN_MINUTE);
     time = time % SECONDS_IN_MINUTE;
-    output.innerText = hours + " hour(s), " + minutes + " minute(s), " + time + " second(s)";
+    output.innerText = hours + ":" + minutes + ":" + time;
 }
 
 function timeConversionSecond() {
