cfd52bf3509861064e9f5fc042b3ee88dcb8fd3e
rdanov <rdanov80@gmail.com>
2020-01-10 15:28:23 +0200

    wd_ps4 voting-form start

---
+++ WD_PS4/voting/php/index.php
@@ -0,0 +1,21 @@
+<?=
+include_once 'function.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>WD_PS4_VOTING</title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<h1 class="content-center">What language do you speak?</h1>
+<div class="container">
+    <form method="post" >
+
+        <input type="submit" value="vote" class="button">
+    </form>
+</div>
+</body>
+</html>
+
